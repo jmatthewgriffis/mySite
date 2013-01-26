24cc00ba39ddee0e4eaf32cdbef977025af52455
J. Matthew Griffis <grifj153@newschool.edu>
2013-01-26 18:00:27 -0500

    In process of styling the footer.

--- frame_bottom.php
+++ frame_bottom.php
@@ -5,34 +5,55 @@ echo '
 
 	<footer>
 
-		<p>&copy; 2012-2013 John Matthew Griffis
-		<br>
-		Lovingly handcrafted from pulverized SNES games,  
-		leftover riffs from the Kind of Blue sessions, 
-		<br>
-		a towel stolen from Douglas Adams, 
-		and the arcane dual-fisted jiu-jitsu of HTML/CSS.
-		</p>
+		<section id="legalAndLogo">
 
-		<a target="_blank" href="https://github.com/jmatthewgriffis">
-			<img src="images/logo_github.png" alt="github logo" title="GitHub" />
-		</a>
+			<section id="leftHalf">
 
-		<a target="_blank" href="http://www.openprocessing.org/user/20132">
-			<img src="images/logo_processing.png" alt="processing logo" title="OpenProcessing" />
-		</a>
+				<p>&copy; 2012-2013 John Matthew Griffis</p>
+			
+			</section><!--end leftHalf-->
 
-		<a target="_blank" href="http://www.linkedin.com/pub/j-matthew-griffis/11/961/7ba">
-			<img src="images/logo_linkedin.png" alt="linkedin logo" title="LinkedIn" />
-		</a>
+			<section id="rightHalf">
 
-		<a target="_blank" href="https://www.facebook.com/j.matthew.griffis">
-			<img src="images/logo_facebook.png" alt="facebook logo" title="Facebook" />
-		</a>
+				<!--LinkedIn-->
+				<a target="_blank" href="http://www.linkedin.com/pub/j-matthew-griffis/11/961/7ba">
+					<img src="images/logo_linkedin.png" alt="linkedin logo" title="LinkedIn" />
+				</a>
 
-		<a href="mailto:jmatthewgriffis@gmail.com">
-			<img src="images/logo_mail.png" alt="email" title="Email" />
-		</a>
+				<!--Facebook-->
+				<a target="_blank" href="https://www.facebook.com/j.matthew.griffis">
+					<img src="images/logo_facebook.png" alt="facebook logo" title="Facebook" />
+				</a>
+
+				<!--Email-->
+				<a href="mailto:jmatthewgriffis@gmail.com">
+					<img src="images/logo_mail.png" alt="email" title="Email" />
+				</a>
+
+				<!--OpenProcessing-->
+				<a target="_blank" href="http://www.openprocessing.org/user/20132">
+					<img src="images/logo_processing.png" alt="processing logo" title="OpenProcessing" />
+				</a>
+
+				<!--GitHub-->
+				<a target="_blank" href="https://github.com/jmatthewgriffis">
+					<img src="images/logo_github.png" alt="github logo" title="GitHub" />
+				</a>
+
+			</section><!--end rightHalf-->
+
+		</section><!--end legalAndLogo-->
+
+		<section id="footerTxt">
+
+			<p>Lovingly handcrafted from pulverized SNES games,  
+			leftover riffs from the Kind of Blue sessions, 
+			<br>
+			a towel stolen from Douglas Adams, 
+			and the arcane dual-fisted jiu-jitsu of HTML/CSS.
+			</p>
+
+		</section><!--end footerTxt-->
 
 	</footer>
 
--- frame_top.php
+++ frame_top.php
@@ -11,7 +11,9 @@ echo '
   
   <title>J. Matthew Griffis - one man, three names, unlimited humor</title>
   <link href="css/style.css" rel="stylesheet" />
-  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css"> <!--Add Google Web Font-->
+
+  <!--Add Google Web Font-->
+  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
  
  </head>
 
