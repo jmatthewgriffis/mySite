df50e31addeb1f99a1301481a9b6cc4535bbfd0f
J. Matthew Griffis <grifj153@newschool.edu>
2013-01-26 16:34:53 -0500

    Added icons/logos to footer, along with some styling.

--- frame_bottom.php
+++ frame_bottom.php
@@ -14,6 +14,26 @@ echo '
 		and the arcane dual-fisted jiu-jitsu of HTML/CSS.
 		</p>
 
+		<a target="_blank" href="https://github.com/jmatthewgriffis">
+			<img src="images/logo_github.png" alt="github logo" title="GitHub" />
+		</a>
+
+		<a target="_blank" href="http://www.openprocessing.org/user/20132">
+			<img src="images/logo_processing.png" alt="processing logo" title="OpenProcessing" />
+		</a>
+
+		<a target="_blank" href="http://www.linkedin.com/pub/j-matthew-griffis/11/961/7ba">
+			<img src="images/logo_linkedin.png" alt="linkedin logo" title="LinkedIn" />
+		</a>
+
+		<a target="_blank" href="https://www.facebook.com/j.matthew.griffis">
+			<img src="images/logo_facebook.png" alt="facebook logo" title="Facebook" />
+		</a>
+
+		<a href="mailto:jmatthewgriffis@gmail.com">
+			<img src="images/logo_mail.png" alt="email" title="Email" />
+		</a>
+
 	</footer>
 
   </div><!--end wrapper-->
