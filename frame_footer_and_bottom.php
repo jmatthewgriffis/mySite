6320b2afe0a8c234255f371b29cad28419e91d68
J. Matthew Griffis <grifj153@newschool.edu>
2013-03-26 12:58:28 -0400

    Some minor tweaks.

--- frame_footer_and_bottom.php
+++ frame_footer_and_bottom.php
@@ -15,6 +15,11 @@ echo '
 
 			<section id="rightHalf">
 
+				<!--Email-->
+				<a href="mailto:jmatthewgriffis@gmail.com">
+					<img src="images/logo_mail2.png" alt="email" title="Email" />
+				</a>
+
 				<!--LinkedIn-->
 				<a target="_blank" href="http://www.linkedin.com/pub/j-matthew-griffis/11/961/7ba">
 					<img id="firstIcon" src="images/logo_linkedin.png" alt="linkedin logo" title="LinkedIn" />
@@ -25,19 +30,14 @@ echo '
 					<img src="images/logo_facebook.png" alt="facebook logo" title="Facebook" />
 				</a>
 
-				<!--Email-->
-				<a href="mailto:jmatthewgriffis@gmail.com">
-					<img src="images/logo_mail.png" alt="email" title="Email" />
+				<!--GitHub-->
+				<a target="_blank" href="https://github.com/jmatthewgriffis">
+					<img src="images/logo_github.png" alt="github logo" title="GitHub" />
 				</a>
 
 				<!--OpenProcessing-->
 				<a target="_blank" href="http://www.openprocessing.org/user/20132">
 					<img src="images/logo_processing.png" alt="processing logo" title="OpenProcessing" />
-				</a>
-
-				<!--GitHub-->
-				<a target="_blank" href="https://github.com/jmatthewgriffis">
-					<img src="images/logo_github.png" alt="github logo" title="GitHub" />
 				</a>
 
 			</section><!--end rightHalf-->
@@ -49,7 +49,7 @@ echo '
 			<p>Lovingly handcrafted from pulverized SNES games,  
 			leftover riffs from the Kind of Blue sessions, 
 			a towel stolen from Douglas Adams, 
-			and the dual-fisted jiu-jitsu of HTML/CSS.
+			and the tri-fisted jiu-jitsu of HTML, CSS and PHP.
 			</p>
 
 		</section><!--end footerTxt-->
