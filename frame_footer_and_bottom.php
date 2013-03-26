9571922a83ce3a1dae01e908adbffed9c5f37026
J. Matthew Griffis <grifj153@newschool.edu>
2013-03-26 17:42:34 -0400

    Revised how I do the footer as well (again for cleaner look on mobile).

--- frame_footer_and_bottom.php
+++ frame_footer_and_bottom.php
@@ -7,40 +7,34 @@ echo '
 
 		<section id="legalAndLogo">
 
-			<section id="leftHalf">
+			<p id="copyright">Copyright &copy; 2012-2013 John Matthew Griffis 
+				
+			<!--Email-->
+			<a href="mailto:jmatthewgriffis@gmail.com">
+				<img src="images/logo_mail2.png" alt="email" title="Email" />
+			</a>
 
-				<p id="copyright">Copyright &copy; 2012-2013 John Matthew Griffis</p>
-			
-			</section><!--end leftHalf-->
+			<!--LinkedIn-->
+			<a target="_blank" href="http://www.linkedin.com/pub/j-matthew-griffis/11/961/7ba">
+				<img id="firstIcon" src="images/logo_linkedin.png" alt="linkedin logo" title="LinkedIn" />
+			</a>
 
-			<section id="rightHalf">
+			<!--Facebook-->
+			<a target="_blank" href="https://www.facebook.com/j.matthew.griffis">
+				<img src="images/logo_facebook.png" alt="facebook logo" title="Facebook" />
+			</a>
 
-				<!--Email-->
-				<a href="mailto:jmatthewgriffis@gmail.com">
-					<img src="images/logo_mail2.png" alt="email" title="Email" />
-				</a>
+			<!--GitHub-->
+			<a target="_blank" href="https://github.com/jmatthewgriffis">
+				<img src="images/logo_github.png" alt="github logo" title="GitHub" />
+			</a>
 
-				<!--LinkedIn-->
-				<a target="_blank" href="http://www.linkedin.com/pub/j-matthew-griffis/11/961/7ba">
-					<img id="firstIcon" src="images/logo_linkedin.png" alt="linkedin logo" title="LinkedIn" />
-				</a>
+			<!--OpenProcessing-->
+			<a target="_blank" href="http://www.openprocessing.org/user/20132">
+				<img src="images/logo_processing.png" alt="processing logo" title="OpenProcessing" />
+			</a>
 
-				<!--Facebook-->
-				<a target="_blank" href="https://www.facebook.com/j.matthew.griffis">
-					<img src="images/logo_facebook.png" alt="facebook logo" title="Facebook" />
-				</a>
-
-				<!--GitHub-->
-				<a target="_blank" href="https://github.com/jmatthewgriffis">
-					<img src="images/logo_github.png" alt="github logo" title="GitHub" />
-				</a>
-
-				<!--OpenProcessing-->
-				<a target="_blank" href="http://www.openprocessing.org/user/20132">
-					<img src="images/logo_processing.png" alt="processing logo" title="OpenProcessing" />
-				</a>
-
-			</section><!--end rightHalf-->
+			</p>
 
 		</section><!--end legalAndLogo-->
 
@@ -49,8 +43,7 @@ echo '
 			<p>Lovingly handcrafted from pulverized SNES games,  
 			leftover riffs from the Kind of Blue sessions, 
 			a towel stolen from Douglas Adams, 
-			and the tri-fisted jiu-jitsu of HTML, CSS and PHP.
-			</p>
+			and the tri-fisted jiu-jitsu of HTML, CSS and PHP.</p>
 
 		</section><!--end footerTxt-->
 
