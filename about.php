4d64227ba86d2e72736e540ebc3b0ddb576df380
J. Matthew Griffis <grifj153@newschool.edu>
2013-03-30 15:51:16 -0400

    Adding to the About page at last.

--- about.php
+++ about.php
@@ -9,21 +9,63 @@
 		<article id="whatPage">
 			<h2><img src="images/people/me3.jpg" alt="This is the About page." title="There's no link - you're already here!"/></a>
 				You need larger images of me<br>with bio and contact info.</h2>
-		</article>
+		</article> <!--end whatPage-->
 
-		<section style="float:left;">
-			<img src="images/people/me.jpg" alt="picture of me" title="You found me!" />
-			<br>
-			<img src="images/people/me2.jpg" alt="picture of me" title="You found me!" />
-		</section>
-		<section>
+		<section id="notDomain">
 
-			<p>Vero dolor butcher, food truck meggings mustache eiusmod accusamus fap incididunt Austin jean shorts odd future banjo fugiat. Proident officia irony pour-over street art stumptown aesthetic. Seitan yr pug, excepteur banh mi voluptate tonx gluten-free eiusmod. Squid cosby sweater incididunt, blog tofu chillwave DIY ethnic laboris deserunt disrupt 90's. Veniam kogi plaid, odd future dolore pug street art ea salvia stumptown literally church-key sunt. Mollit ea swag, magna letterpress vinyl qui thundercats freegan. Hashtag beard raw denim, keytar fashion axe lomo tattooed cardigan yr helvetica gluten-free irony seitan.
+			<article id="mePics" title="Don't look at me!">
+			</article> <!--end mePics-->
 
-	Chillwave flannel brunch squid tempor, aute eu vero. Dolore stumptown neutra, exercitation mixtape you probably haven't heard of them quis reprehenderit ugh vinyl nesciunt ex flexitarian vegan. Flexitarian american apparel ea, DIY elit plaid incididunt bicycle rights food truck wayfarers fashion axe. Mollit synth id, sriracha pickled umami ethnic accusamus yr Austin artisan blue bottle. Street art butcher elit banksy polaroid jean shorts. Readymade sriracha gastropub hoodie pour-over, 90's +1 four loko. Fingerstache fixie four loko proident fanny pack ethnic occupy thundercats anim, gastropub scenester.
+			<article id="text">
 
-	In bushwick organic ex hoodie gentrify, pitchfork truffaut cred pour-over farm-to-table seitan kogi. American apparel master cleanse odio aesthetic echo park. Enim chambray ex labore cillum. Sriracha selvage flannel fixie id sed. Whatever fashion axe banksy, kogi hashtag vice odio swag fingerstache craft beer brooklyn deep v polaroid shoreditch. Post-ironic messenger bag 3 wolf moon non aliqua. Mcsweeney's four loko pug wayfarers, ethnic hoodie blog street art sapiente lo-fi duis placeat.</p>
-		</section>
+				<section id="logos"> 
+						
+					<!--Email-->
+					<a href="mailto:jmatthewgriffis@gmail.com">
+						<img src="images/logos/logo_mail2.png" alt="email" title="Email" />
+					</a>
+
+					<!--LinkedIn-->
+					<a target="_blank" href="http://www.linkedin.com/pub/j-matthew-griffis/11/961/7ba">
+						<img src="images/logos/logo_linkedin.png" alt="linkedin logo" title="LinkedIn" />
+					</a>
+
+					<!--Facebook-->
+					<a target="_blank" href="https://www.facebook.com/j.matthew.griffis">
+						<img src="images/logos/logo_facebook.png" alt="facebook logo" title="Facebook" />
+					</a>
+
+					<!--GitHub-->
+					<a target="_blank" href="https://github.com/jmatthewgriffis">
+						<img src="images/logos/logo_github.png" alt="github logo" title="GitHub" />
+					</a>
+
+					<!--OpenProcessing-->
+					<a target="_blank" href="http://www.openprocessing.org/user/20132">
+						<img src="images/logos/logo_processing.png" alt="processing logo" title="OpenProcessing" />
+					</a>
+
+					</p>
+
+				</section> <!--end logos-->
+
+				<p>Vero dolor butcher, food truck meggings mustache eiusmod accusamus fap incididunt Austin jean shorts odd future banjo fugiat. Proident officia irony pour-over street art stumptown aesthetic. Seitan yr pug, excepteur banh mi voluptate tonx gluten-free eiusmod. Squid cosby sweater incididunt, blog tofu chillwave DIY ethnic laboris deserunt disrupt 90's. Veniam kogi plaid, odd future dolore pug street art ea salvia stumptown literally church-key sunt. Mollit ea swag, magna letterpress vinyl qui thundercats freegan. Hashtag beard raw denim, keytar fashion axe lomo tattooed cardigan yr helvetica gluten-free irony seitan.
+
+		Chillwave flannel brunch squid tempor, aute eu vero. Dolore stumptown neutra, exercitation mixtape you probably haven't heard of them quis reprehenderit ugh vinyl nesciunt ex flexitarian vegan. Flexitarian american apparel ea, DIY elit plaid incididunt bicycle rights food truck wayfarers fashion axe. Mollit synth id, sriracha pickled umami ethnic accusamus yr Austin artisan blue bottle. Street art butcher elit banksy polaroid jean shorts. Readymade sriracha gastropub hoodie pour-over, 90's +1 four loko. Fingerstache fixie four loko proident fanny pack ethnic occupy thundercats anim, gastropub scenester.
+
+		In bushwick organic ex hoodie gentrify, pitchfork truffaut cred pour-over farm-to-table seitan kogi. American apparel master cleanse odio aesthetic echo park. Enim chambray ex labore cillum. Sriracha selvage flannel fixie id sed. Whatever fashion axe banksy, kogi hashtag vice odio swag fingerstache craft beer brooklyn deep v polaroid shoreditch. Post-ironic messenger bag 3 wolf moon non aliqua. Mcsweeney's four loko pug wayfarers, ethnic hoodie blog street art sapiente lo-fi duis placeat.</p>
+
+			</article> <!--end text-->
+
+			<article style="display:inline-block; vertical-align:top; margin:1em; border:solid 1px black;"><p>Yo it's dummy text.</p></article>
+
+		</section> <!--end notDomain-->
+
+		<article id="domain">
+			<p>Domain Map | February 2013</p>
+			<img src="images/background/domain_map.png" alt="Domain Map" title="My interests, encoded" />
+		</article> <!--end domain-->
+
 		<div id="mePic" style="clear:both;">
 		</div><!--end mePic-->
 
