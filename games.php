2f6b2b8df268670720c8adb79c3a6084881a78d7
J. Matthew Griffis <grifj153@newschool.edu>
2014-06-11 16:28:50 -0600

    Minor updates.

--- games.php
+++ games.php
@@ -15,9 +15,9 @@
 
 		<!-- Rifftide -->
 		<article class="thing game">
-			<img src="images/games/rifftide_logo.png" alt="Rifftide" title="Music flows. Ride the tide." />
-			<h3>Rifftide</h3>
+			<img src="images/games/rifftide_logo.png" alt="Rifftide" />
 			<div class="details">
+				<h3>Rifftide</h3>
 			<p class="specs">
 				Spring 2014<br>
 				<a href="writing/rifftide__production_doc.pdf">about</a>
@@ -34,10 +34,10 @@
 		<!-- The Doorway Experience -->
 		<article class="thing game">
 			<!-- <a href="images/games/screen__doorway_experience__full.png"> -->
-				<img src="images/games/screen__doorway_experience__crop.png" alt="Experience Doorways" title="You've never seen doorways like this. Probably." />
+				<img src="images/games/screen__doorway_experience__crop.png" alt="Experience Doorways" />
 			<!-- </a> -->
-			<h3>The Doorway Experience: Experience Doorways</h3>
 			<div class="details">
+				<h3>The Doorway Experience: Experience Doorways</h3>
 			<p class="specs">
 				April 2014<br>
 				<a href="games/The_Doorway_Experience--Experience_Doorways__Mac_build.zip">download (Mac)</a> | 
@@ -52,10 +52,10 @@
 		<!-- Project Cheese -->
 		<article class="thing game">
 			<!-- <a href="images/games/screen__project_cheese__full.jpg"> -->
-				<img src="images/games/screen__project_cheese__crop.jpg" alt="Project Cheese" title="Dangerously Cheesy" />
+				<img src="images/games/screen__project_cheese__crop.jpg" alt="Project Cheese" />
 			<!-- </a> -->
-			<h3>Dust in the Wind: Project Cheese</h3>
 			<div class="details">
+				<h3>Dust in the Wind: Project Cheese</h3>
 			<p class="specs">
 				March 2014<br>
 				<a target="_blank" href="http://vimeo.com/90924630">watch video</a> | 
@@ -70,10 +70,10 @@
 		<!-- Space Odyssey 2 -->
 		<article class="thing game">
 			<!-- <a href="images/games/screen__space_odyssey_2__full.png"> -->
-				<img src="images/games/screen__space_odyssey_2__crop.png" alt="Space Odyssey 2" title="Pew pew" />
+				<img src="images/games/screen__space_odyssey_2__crop.png" alt="Space Odyssey 2" />
 			<!-- </a> -->
-			<h3>Space Odyssey 2: Michael Kahane's Defection</h3>
 			<div class="details">
+				<h3>Space Odyssey 2: Michael Kahane's Defection</h3>
 			<p class="specs">
 				December 2013<br>
 				<a href="games/Space_Odyssey_2.app.zip">download</a> | 
@@ -88,10 +88,10 @@
 		<!-- Early Thesis Prototype -->
 		<article class="thing game">
 			<!-- <a href="images/games/screen__thesis_early_prototype__full.png"> -->
-				<img src="images/games/screen__thesis_early_prototype__crop.png" alt="Thesis--Early Prototype" title="Graphics not included." />
+				<img src="images/games/screen__thesis_early_prototype__crop.png" alt="Thesis--Early Prototype" />
 			<!-- </a> -->
-			<h3>Thesis [semester 1 prototype]</h3>
 			<div class="details">
+				<h3>Thesis [semester 1 prototype]</h3>
 			<p class="specs">
 				December 2013<br>
 				<a href="games/Griffis_Thesis_prototype_Mac.zip">download</a> | 
@@ -102,49 +102,13 @@
 		</div>
 		</article>
 
-		<!-- Collab: NWS ("Breath and Brass") -->
-		<article class="thing game">
-			<!-- <a href="images/games/screen__breath_and_brass__full.jpg"> -->
-				<img src="images/games/screen__breath_and_brass__crop.jpg" alt="Breath and Brass" title="Breath and Brass" />
-			<!-- </a> -->
-			<h3>Breath and Brass</h3>
-			<div class="details">
-			<p class="specs">
-				November 2013<br>
-				<a target="_blank" href="http://vimeo.com/87206691">watch video</a> | 
-				about 
-				<a target="_blank" href="http://parsonsnws.wordpress.com/2013/12/09/breath-and-brass/">[1]</a> 
-				<a href="writing/breath_and_brass__documentation.pdf">[2]</a> | 
-				<a target="_blank" href="https://github.com/LordOfCorners/SymphonyCollab">code</a>
-			</p>
-			<p>Not a game, but in the interactive category, this Parsons group project was a collaboration with Miami's New World Symphony. Michael Kahane, Quincy Bock, Mauricio Sanchez-Duque, Francesca Castelli, and I developed responsive, generative software for use in concert.</p>
-			<p class="specs">Made with openFrameworks and Arduino</p>
-		</div>
-		</article>
-
-		<!-- Super Sack Race -->
-		<article class="thing game">
-			<!-- <a href="images/games/screen__super_sack_race__full.png"> -->
-				<img src="images/games/screen__super_sack_race__crop.png" alt="Super Sack Race" title="Rucksacks are in this year." />
-			<!-- </a> -->
-			<h3>Super Sack Race</h3>
-			<div class="details">
-			<p class="specs">
-				July 2013<br>
-				<a target="_blank" href="http://www.usanetwork.com/summercamp/supersackrace">play</a>
-			</p>
-			<p>Worst sack race course ever! Jump and use powerups to avoid the many obstacles while getting as far and scoring as many points as you can. I helped develop this commission from NBCUniversal while working at TreSensa.</p>
-			<p class="specs">Made with TGE (TreSensa Game Engine)</p>
-		</div>
-		</article>
-
 		<!-- What Lies Beyond -->
 		<article class="thing game">
 			<!-- <a href="images/games/screenshot__what_lies_beyond__full.png"> -->
-				<img src="images/games/screenshot__what_lies_beyond__cropped.png" alt="What Lies Beyond" title="Begin" />
+				<img src="images/games/screenshot__what_lies_beyond__cropped.png" alt="What Lies Beyond" />
 			<!-- </a> -->
-			<h3>What Lies Beyond</h3>
 			<div class="details">
+				<h3>What Lies Beyond</h3>
 			<p class="specs">
 				May 2013<br>
 				<a href="games/What_Lies_Beyond__Matt_Griffis.zip">download</a> | 
@@ -159,49 +123,14 @@
 		</div>
 		</article>
 
-		<!-- Dicksit -->
-		<!-- http://www.humorandcode.com/spring13/dicksit -->
-		<article class="thing game">
-			<!-- <a href="images/games/screen__dicksit__full.png"> -->
-				<img src="images/games/screen__dicksit__crop.png" alt="Dicksit" title="NSFW?" />
-			<!-- </a> -->
-			<h3>Dicksit [prototype]</h3>
-			<div class="details">
-			<p class="specs">
-				May 2013<br>
-				<a target="_blank" href="https://vimeo.com/66433091">watch video</a>
-			</p>
-			<p>When Twitter meets 4chan, the result is a game show you won't want to show your family. Contestants choose an image from a random 4chan thread to respond to a Tweet from an audience-supplied prompt. <a target="_blank" href="http://www.humorandcode.com/spring13/dicksit">Read the original write-up</a> for this Parsons group prototype.</p>
-			<p class="specs">Made with Node.js, WebSockets, HTML, CSS, and the Twitter and 4chan APIs</p>
-		</div>
-		</article>
-
-		<!-- This Connected Life -->
-		<!-- http://www.humorandcode.com/spring13/this-connected-life-satirical-software-2 -->
-		<article class="thing game">
-			<!-- <a href="images/games/screenshot__this_connected_life__full.png"> -->
-				<img src="images/games/screenshot__this_connected_life__cropped.png" alt="This Connected Life" title="I want to touch it!" />
-			<!-- </a> -->
-			<h3>This Connected Life [prototype]</h3>
-			<div class="details">
-			<p class="specs">
-				May 2013<br>
-				<a href="games/This_Connected_Life.zip">download</a> | 
-				<a target="_blank" href="https://github.com/jmatthewgriffis/Humor-and-Code-2013/tree/master/of_v0.7.4_osx_release/apps/myApps/This_Connected_Life">code</a>
-			</p>
-			<p>Those smartphones sure are great. You'll never miss another call, text, email, post, website, game, book, movie, show, financial transaction, or anything else. Whether you're asleep, awake, entangled, or infirm, they will find you! No question: you're leading This Connected Life.</p>
-			<p class="specs">Made with openFrameworks</p>
-		</div>
-		</article>
-
 		<!-- Don't Play Me Bro -->
 		<!-- http://www.humorandcode.com/spring13/dont-play-me-bro-satirical-software-1 -->
 		<article class="thing game">
 			<!-- <a href="images/games/screenshot__dont_play_me_bro.png"> -->
-				<img src="images/games/screenshot__dont_play_me_bro.png" alt="Don't Play Me Bro" title="Dude, Bro" />
+				<img src="images/games/screenshot__dont_play_me_bro.png" alt="Don't Play Me Bro" />
 			<!-- </a> -->
-			<h3>Don't Play Me Bro [prototype]</h3>
 			<div class="details">
+				<h3>Don't Play Me Bro [prototype]</h3>
 			<p class="specs">
 				May 2013<br>
 				<a href="games/Dont_Play_Me_Bro.zip">download</a> | 
@@ -215,10 +144,10 @@
 		<!-- Building Worlds Unity stuff -->
 		<article class="thing game">
 			<!-- <a href="images/games/screen__building_worlds_final__full.png"> -->
-				<img src="images/games/screen__building_worlds_final__crop.png" alt="Experiments in Unity3D" title="Maybe I'll just go back to bed." />
+				<img src="images/games/screen__building_worlds_final__crop.png" alt="Experiments in Unity3D" />
 			<!-- </a> -->
-			<h3>Experiments in Unity3D</h3>
 			<div class="details">
+				<h3>Experiments in Unity3D</h3>
 			<p class="specs">
 				Spring 2013<br>
 				<a href="games/unity_bw_list.html">play vignettes</a> | 
@@ -232,10 +161,10 @@
 		<!-- Space Odyssey -->
 		<article class="thing game">
 			<!-- <a href="images/games/space_odyssey.png"> -->
-				<img src="images/games/space_odyssey.png" alt="Space Odyssey" title="Pew pew" />
+				<img src="images/games/space_odyssey.png" alt="Space Odyssey" />
 			<!-- </a> -->
-			<h3>Space Odyssey [prototype]</h3>
 			<div class="details">
+				<h3>Space Odyssey [prototype]</h3>
 			<p class="specs">
 				March 2013<br>
 				<a target="_blank" href="http://www.openprocessing.org/sketch/93227">play and see code</a>
@@ -245,32 +174,13 @@
 		</div>
 		</article>
 
-		<!-- Space Pollution -->
-		<article class="thing game">
-			<!-- <a href="images/games/spacejunk.png"> -->
-				<img src="images/games/spacejunk.png" alt="Space Pollution Needs Solution" title="It really does." />
-			<!-- </a> -->
-			<h3>Space Pollution [prototype]</h3>
-			<div class="details">
-			<p class="specs">
-				February 2013<br>
-				<a href="games/spacejunk.zip">zip w/ code</a> | 
-				<a target="_blank" href="http://www.openprocessing.org/sketch/90457">play stage 1</a> | 
-				<a href="writing/spacejunk_documentation.pdf">about</a> | 
-				<a href="music.php">music</a>
-			</p>
-			<p>Who knew going into orbit was so perilous? Owen Herterich, Michael Kahane and I found out when we created this prototype. To run the two-stage version with music, use <a target="_blank" href="http://processing.org/download/">Processing</a> to open spaceJunk.pde in the zip archive and press Play.</p>
-			<p class="specs">Made with Processing</p>
-		</div>
-		</article>
-
 		<!-- Art Thief: TG -->
 		<article class="thing game">
 			<!-- <a href="images/games/art_thief_training_grounds.png"> -->
-				<img src="images/games/art_thief_training_grounds.png" alt="Art Thief: Training Grounds" title="Not pictured: George Clooney" />
+				<img src="images/games/art_thief_training_grounds.png" alt="Art Thief: Training Grounds" />
 			<!-- </a> -->
-			<h3>Art Thief: Training Grounds</h3>
 			<div class="details">
+				<h3>Art Thief: Training Grounds</h3>
 			<p class="specs">
 				December 2012<br>
 				<a href="games/art_thief_training_grounds.zip">download</a> | 
@@ -285,10 +195,10 @@
 		<!-- How to Play -->
 		<article class="thing game">
 			<!-- <a href="images/games/howtoplay.png"> -->
-				<img src="images/games/howtoplay.png" alt="How to Play" title="So mysterious..." />
+				<img src="images/games/how_to_play.png" alt="How to Play" />
 			<!-- </a> -->
-			<h3>How to Play</h3>
 			<div class="details">
+				<h3>How to Play</h3>
 			<p class="specs">
 				November 2012<br>
 				<a target="_blank" href="http://www.openprocessing.org/sketch/79599">play and see code</a> | 
@@ -299,62 +209,13 @@
 		</div>
 		</article>
 
-		<!-- Arduino Invader -->
-		<article class="thing game">
-			<!-- <a href="images/games/arduino_invader.png"> -->
-				<img src="images/games/arduino_invader.png" alt="Arduino Invader" title="It came alone, wielding great power." />
-			<!-- </a> -->
-			<h3>Arduino Invader</h3>
-			<div class="details">
-			<p class="specs">
-				October 2012<br>
-				<a target="_blank" href="http://www.openprocessing.org/sketch/77167">play and see code</a> | 
-				<a href="writing/arduino_invader_documentation.pdf">about</a>
-			</p>
-			<p>The aliens are invading! Oh, there's just one. There comes a time in a man's life when he decides to build his own arcade game controller while learning about electronics and physical computing. For me, that time came during my studies at Parsons the New School for Design.</p>
-			<p class="specs">Made with Processing and Arduino</p>
-		</div>
-		</article>
-
-		<!-- Frustration -->
-		<article class="thing game">
-			<!-- <a href="images/games/frustration.png"> -->
-				<img src="images/games/frustration.png" alt="Frustration" title="Frustration" />
-			<!-- </a> -->
-			<h3>Frustration</h3>
-			<div class="details">
-			<p class="specs">
-				September 2012<br>
-				<a target="_blank" href="http://www.openprocessing.org/sketch/68684">play and see code</a>
-			</p>
-			<p>Click on the ball to score points. It's simple enough. I created this "game" for my studies at Parsons the New School for Design. The assignment was to make something expressing (or, perhaps, inducing) an emotion. Results from playtesting are positive.</p>
-			<p class="specs">Made with Processing</p>
-		</div>
-		</article>
-
-		<!-- Bounce -->
-		<article class="thing game">
-			<!-- <a href="images/games/bounce.png"> -->
-				<img src="images/games/bounce.png" alt="Bounce" title="Bounce it like it's hot." />
-			<!-- </a> -->
-			<h3>Bounce</h3>
-			<div class="details">
-			<p class="specs">
-				August 2012<br>
-				<a target="_blank" href="http://www.openprocessing.org/sketch/68200">play and see code</a>
-			</p>
-			<p>A physics platform game with balls. Use the mouse to move one ball and bounce the other off of it. Press 's' on the title screen for Schmaltz mode. I created this for my final project in the Code section of the pre-program "Bootcamp" at Parsons the New School for Design.</p>
-			<p class="specs">Made with Processing</p>
-		</div>
-		</article>
-
 		<!-- Platformal Ware -->
 		<article class="thing game">
 			<!-- <a href="images/games/platformal_ware.png"> -->
-				<img src="images/games/platformal_ware.png" alt="Platformal Ware" title="A foggy day in Platform Town." />
+				<img src="images/games/platformal_ware.png" alt="Platformal Ware" />
 			<!-- </a> -->
-			<h3>Platformal Ware</h3>
 			<div class="details">
+				<h3>Platformal Ware</h3>
 			<p class="specs">
 				December 2011<br>
 				<a href="games/platformal_ware_-_a_demo.zip">download</a> | 
